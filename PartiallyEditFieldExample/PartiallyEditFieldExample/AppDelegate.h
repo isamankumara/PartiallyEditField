12bc25fb2f08d396df255fd91abf1fc2979237e8
isaman kumara <me@isamankumara.com>
2017-09-12 21:22:00 +0530

    project setting changed

---
+++ PartiallyEditFieldExample/PartiallyEditFieldExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PartiallyEditFieldExample
+//
+//  Created by Saman kumara on 9/12/17.
+//  Copyright © 2017 Saman Weerasinghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PartiallyEditFieldExample/PartiallyEditFieldExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PartiallyEditFieldExample
+//
+//  Created by Saman kumara on 9/12/17.
+//  Copyright © 2017 Saman Weerasinghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
